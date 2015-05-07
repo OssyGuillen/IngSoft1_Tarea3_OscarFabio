fe1e374b735e485579d05de24d829b4f49361c15
OssyGuillen <guillen.usb@gmail.com>
2015-05-06 20:10:16 -0430

    Archivos bases 2

---
+++ BilleteraElectronica/BilleteraElectronica.py
@@ -0,0 +1,11 @@
+'''
+Created on 04/05/2015
+@title Tarea 3: Billetera Electronica
+
+@author: Fabio Castro 10-10132
+@author: Oscar Guillen 11-11264
+
+@Description
+            Implementacion de la clase billetera electronica.
+            Estructura creada como servicio de pago...
+'''
---
+++ BilleteraElectronica/recarga.py
@@ -0,0 +1,10 @@
+'''
+Created on 04/05/2015
+@title Clase Recarga
+
+@author: Fabio Castro 10-10132
+@author: Oscar Guillen 11-11264
+
+@Description
+            Implementacion de la estructura recarga
+'''
